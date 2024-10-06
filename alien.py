88e5b25564da3f5e518c1ed833a54fcf075d97e3
Nicholas Reeves <n.reeves429@csu.fullerton.edu>
2024-10-05 22:07:27 -0700

    Initial commit

---
+++ alien.py
@@ -0,0 +1,75 @@
+import pygame as pg
+from vector import Vector
+from point import Point
+from laser import Laser 
+from pygame.sprite import Sprite
+from timer import Timer
+from random import randint
+
+class Alien(Sprite):
+    # alien_images0 = [pg.image.load(f"images/alien0{n}.png") for n in range(2)]
+    alien_images0 = [pg.image.load("images/alien-1a.png"),pg.image.load("images/alien-1b.png")]
+    alien_images1 = [pg.image.load("images/alien-2a.png"),pg.image.load("images/alien-2b.png")]
+    alien_images2 = [pg.image.load("images/alien-3a.png"),pg.image.load("images/alien-3b.png")]
+    alien_images = [alien_images0, alien_images1, alien_images2]
+    alien_explosion = pg.image.load("images/alien-explosion.png")  # fill in explosion images here
+
+    def __init__(self, ai_game, v): 
+        super().__init__()
+        self.ai_game = ai_game
+        self.screen = ai_game.screen
+        self.v = v
+
+        type = randint(0, 2)
+        self.timer = Timer(images=Alien.alien_images[type], delta=900, start_index=type % 2)
+        # self.explosion_timer = Timer(images=Alien.alien.explosion_images, start_index=Alien.n % 2,
+        #                              loop_continuously=False)
+        self.image = self.timer.current_image()
+        print(self.image)
+        self.rect = self.image.get_rect()
+
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        self.is_dying = False
+        self.death_timer = 0
+        self.is_dead = False
+
+    def check_edges(self):
+        sr = self.screen.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        r = self.rect 
+        return self.x + self.rect.width >= sr.right or self.x <= 0
+
+    def update(self):
+        if self.is_dying != True and  self.is_dead != True:
+            self.x += self.v.x
+            self.y += self.v.y
+        self.image = self.timer.current_image()
+        if self.is_dying:
+            self.image = self.alien_explosion
+            self.death_timer += 1
+            if self.death_timer >= 15:
+                self.is_dead = True
+        self.draw()
+        
+        
+    def draw(self): 
+        self.rect.x = self.x
+        self.rect.y = self.y
+        self.screen.blit(self.image, self.rect)
+
+
+def main():
+    print('\n run from alien_invasions.py\n')
+
+if __name__ == "__main__":
+    main()
+
+
+
+
---
+++ sound.py
@@ -0,0 +1,83 @@
+import pygame as pg 
+import time
+from random import randint
+
+
+class Sound:
+    def __init__(self): 
+        self.pickup = pg.mixer.Sound('sounds/pickup.wav')
+        self.gameover = pg.mixer.Sound('sounds/gameover.wav')
+
+        self.ah = pg.mixer.Sound('sounds/Death_Sounds/ah.mp3')
+        self.aliendeath = pg.mixer.Sound('sounds/Death_Sounds/aliendeath.mp3')
+        self.cutitout = pg.mixer.Sound('sounds/Death_Sounds/cutitout.mp3')
+        self.darnit = pg.mixer.Sound('sounds/Death_Sounds/darn.mp3')
+        self.hey = pg.mixer.Sound('sounds/Death_Sounds/hey.mp3')
+        self.nothappy = pg.mixer.Sound('sounds/Death_Sounds/nothappy.mp3')
+        self.ooh = pg.mixer.Sound('sounds/Death_Sounds/ooh.mp3')
+        self.ow = pg.mixer.Sound('sounds/Death_Sounds/ow.mp3')
+        self.owww = pg.mixer.Sound('sounds/Death_Sounds/owwww.mp3')
+        self.stop = pg.mixer.Sound('sounds/Death_Sounds/stop.mp3')
+        self.wilhelm = pg.mixer.Sound('sounds/Death_Sounds/wilhelm.mp3')
+        self.yeouch = pg.mixer.Sound('sounds/Death_Sounds/yeouch.mp3')
+
+        pg.mixer.music.load('sounds/ride_of_the_valkyries.mp3')
+        pg.mixer.music.set_volume(0.1)
+                                             
+    def play_background(self): 
+        pg.mixer.music.play(-1, 0.0)
+        self.music_playing = True
+        
+    def play_pickup(self): 
+        if self.music_playing: self.pickup.play()
+    
+    def play_deathsound(self):
+        randline = randint(1,1000)
+        
+        if 1<= randline <= 550:
+            pass
+        elif 551 <= randline <=600 :
+            self.ah.play()
+        elif 601 <= randline <=650 :
+            self.ow.play()
+        elif 651 <= randline <= 700:
+            self.ooh.play()
+        elif 701 <= randline <= 750:
+            self.hey.play()
+        elif 751 <= randline <= 800:
+            self.yeouch.play()
+        elif 801 <= randline <= 850:
+            self.owww.play()
+        elif 851 <= randline <= 880:
+            self.cutitout.play()
+        elif 881 <= randline <=920 :
+            self.stop.play()
+        elif 921 <= randline <=970 :
+            self.darnit.play()
+        elif 971 <= randline <=990 :
+            self.nothappy.play()
+        elif 991 <= randline <= 995:
+            self.aliendeath.play()
+        elif 996 <= randline <= 1000:
+            self.wilhelm.play()
+        
+    def play_gameover(self):
+        if self.music_playing: 
+            self.stop_background()
+            self.gameover.play()
+            time.sleep(3.0)       # sleep until game over sound has finished
+        
+    def toggle_background(self):
+        if self.music_playing: 
+            self.stop_background()
+        else:
+            self.play_background()
+        self.music_playing = not self.music_playing
+        
+    def stop_background(self): 
+        pg.mixer.music.stop()
+        self.music_playing = False 
+    
+        
+    
+    
